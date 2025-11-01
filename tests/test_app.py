03cc6c5e1abcbf368154aba209ae6b632a469020
atlasjx <diogofatia@gmail.com>
2025-11-01 15:09:05 +0000

    Test fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,4 +2,4 @@ def test_app():
     from app import app
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"gha-example"
+    assert response.data == b"ads-backend"
